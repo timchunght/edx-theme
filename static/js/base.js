dc08277f44e0c4ef63563b74ef17850bafbc27e8
Matej Grozdanović <grozdanowski@gmail.com>
2016-02-24 18:52:49 +0100

    start working on multilayered theming

---
+++ static/js/base.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+	// site mobile nav toggle
+	$('#site-menu-toggle').on('click', function() {
+		$(this).toggleClass('a--toggled');
+		$('#slide-menu').toggleClass('a--active');
+	});
+
+	// execute css on-load animations
+	$('.a--animated').addClass("a--do-animate");
+
+	function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+	$(window).scroll(function () {
+	   $('.a--animate-on-scroll.a--out-animation').each(function () {
+	      if (isScrolledIntoView(this) === true) {
+	          $(this).addClass('a--in-view');
+	      } else {
+					$(this).removeClass('a--in-view');
+				}
+	   });
+	});
+
+	$(window).scroll(function () {
+	   $('.a--animate-on-scroll.a--no-out-animation').each(function () {
+	      if (isScrolledIntoView(this) === true) {
+	          $(this).addClass('a--in-view');
+	      }
+	   });
+	});
+
+	$('.a--reveal-on-load').each(function() {
+		$(this).addClass('a--revealed');
+	});
+});
